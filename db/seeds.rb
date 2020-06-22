b8bfc05ed820966f128a55e4b73d7bb2c2b785f4
Mark-Ball <markball111@gmail.com>
2020-06-22 16:14:56 +1000

    put api key in credentials and worked on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# access api key
+key = Rails.application.credentials.api_key
+
 # seed Agencies table
-agencies = HTTParty.get('https://www.realhubapp.com/api/v2/agencies')
-agencies = JSON.parse(agencies.body)
-for agency in agencies
-    Agency.create(name: agency['title'])
-end
-puts agencies seeded
+# agencies = HTTParty.get('https://app.realhublive.com/api/v2/agencies',
+#     headers: {'x-api-token': key}
+# )
+# i = 1
+# for agency in agencies
+#     Agency.create(title: agency['title'])
+#     puts "agency created #{i}/10"
+#     i += 1
+# end
+# puts 'all agencies seeded'
 
 # seed Campaigns table
-campaigns = HTTParty.get('https://www.realhubapp.com/api/v2/campaigns')
-campaigns = JSON.parse(campaigns.body)
-for campaign in campaigns
-    Campaign.create(
-        unit_number: campaign['unit_number'],
-        street_number: campaign['street_number'],
-        street_name: campaign['street_name'],
-        suburb_name: campaign['suburb_name']
-    )
-end
-puts campaigns seeded
+# campaigns = HTTParty.get('https://app.realhublive.com/api/v2/campaigns',
+#     headers: {'x-api-token': key}
+# )
+# i = 1
+# for campaign in campaigns
+#     Campaign.create(
+#         unit_number: campaign['unit_number'],
+#         street_number: campaign['street_number'],
+#         street_name: campaign['street_name'],
+#         suburb_name: campaign['suburb_name']
+#     )
+#     puts "campaign created #{i}/20"
+#     i += 1
+# end
+# puts 'all campaigns seeded'
 
 # seed Statuses table
-statuses = HTTParty.get('https://www.realhubapp.com/api/v2/statuses')
-statuses = JSON.parse(statuses.body)
-for status in statuses
-    Status.create(title: status['title'])
-end
-puts statuses seeded
+# statuses = HTTParty.get('https://app.realhublive.com/api/v2/statuses',
+#     headers: {'x-api-token': key}
+# )
+# i = 1
+# for status in statuses
+#     Status.create(title: status['title'])
+#     puts "status created #{i}/20"
+# end
+# puts 'all statuses seeded'
 
 # seed Orders table
-orders = HttParty.get('https://www.realhubapp.com/api/v2/orders')
-orders = JSON.parse(orders.body)
+orders = HTTParty.get('https://app.realhublive.com/api/v2/orders',
+    headers: {'x-api-token': key}
+)
+i = 1
 for order in orders
-        Order.create(
-        agency_id: order['agency_id'],
-        status_id: order['status_id'],
-        campaign_id: order['campaign_id'],
-        title: 'A4 Brochures',
-        quantity: 100
-    )
+        createdOrder = Order.create(
+            agency_id: order['agency_id'],
+            status_id: order['status_id'],
+            campaign_id: order['campaign_id'],
+            title: order['title'],
+            quantity: order['total']
+        )
+    puts createdOrder
+    puts "order created #{i}/50"
+    i += 1
 end
-puts orders seeded
+puts 'all orders seeded'
