34f0968704ae29e20b37accd43a4070866e029d0
Mark-Ball <markball111@gmail.com>
2020-06-26 13:00:11 +1000

    removed print statement from order items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,7 +3,6 @@ class OrderItemsController < ApplicationController
 
     def change_status
         item = OrderItem.find(params['id']).update(status_id: params['status'])
-        puts item
-        render json: { message: 'ok' }
+        render json: item
     end
 end
